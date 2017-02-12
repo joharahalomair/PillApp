a864df23dcd8b7510eaf91684fb333bf1d0065b2
Christian Amaya <cjamaya@uci.edu>
2017-02-12 14:08:40 -0800

    Added a main dhasboard page. Both Day Name and the Date will change depending on the actual date. Managed to generate a medication list based off of the size of an array. Still needs some styling because it looks really plain. Need to incorporate day switching mechanic.

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+} 
+
+if(mm<10) {
+    mm='0'+mm
+} 
+today = mm+'/'+dd+'/'+yyyy;
+
+$(document).ready(function getDayName() {
+    $("#dayName").html(weekday[d.getDay()]);
+});
+
+$(document).ready(function getCurrentDate() {
+    $("#currentDate").html(today);
+});
+
+$(document).ready(function generateMedList() {
+    for (i = 0; i < weekday.length; i++) {
+        $("#medicationList").append(
+            "<div class='row med-item col-xs-12'> \
+                <div class='row'> \
+                    <label>8:00 am</label> \
+                </div> \
+                <div class='row'> \
+                    <img class='col-xs-4 green-check' src='assets/greenCheck.png'> \
+                    <div class='col-xs-8'>Adderall</div> \
+                </div> \
+                <div class='row'> \
+                    <img class='col-xs-4 green-check' src='assets/greenCheck.png'> \
+                    <div class='col-xs-8'>Adderall</div> \
+                </div> \
+            </div>"
+        );
+    }
+});
